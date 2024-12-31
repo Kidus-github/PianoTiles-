7a26e2b48514ca5e122c1b2055bcf8cd86c0043a
Kidus-github <kidus.muluwork@gmail.com>
2024-12-31 23:58:18 +0300

    worked on this personal project for fun

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+// import 'package:piano_tiles/screens/home_screen/home_screen.dart';
+import 'package:piano_tiles/screens/splash_screen/splash_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
